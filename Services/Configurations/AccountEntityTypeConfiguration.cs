c276749b70151b172000c5401fe138d416d1c1c3
Aleksey Belov <belovnow@gmail.com>
2021-08-09 13:52:43 +0300

    Created Entity Type Configurations for Wallet, Account and Operation
    * Added Logger to ApplicationContext

---
+++ Services/Configurations/AccountEntityTypeConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Model;
+
+namespace AccountantAppWebAPI
+{
+	public class AccountEntityTypeConfiguration : IEntityTypeConfiguration<Account>
+	{
+		public void Configure(EntityTypeBuilder<Account> builder)
+		{
+			builder.HasData(
+				new Account {Id = 1, Amount = 55000, Name = "Зарплата", Type = AccountType.Income},
+				new Account {Id = 2, Amount = 3535.63, Name = "Продукты", Type = AccountType.Expense},
+				new Account {Id = 3, Amount = 0, Name = "Развлечения", Type = AccountType.Expense});
+		}
+	}
+}
